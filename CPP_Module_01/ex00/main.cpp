3239fc6fffec6b5d0bc8642e0791ba2186028ed5
elhousseine lakhfif <elakhfif@c2r5p10.1337.ma>
2023-12-08 16:12:55 +0100

    done

--- CPP_Module_01/ex00/main.cpp
+++ CPP_Module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 06:41:44 by elakhfif          #+#    #+#             */
-/*   Updated: 2023/11/24 02:18:13 by elakhfif         ###   ########.fr       */
+/*   Updated: 2023/12/08 16:11:39 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ int	main()
 
 	i = -1;
 	Zombie *zombie = newZombie("tarzan");
-	while (++i < 3)
-		zombie->announce();
+	zombie->announce();
 	randomChump("jane");
 	delete zombie;
 	return (0);
