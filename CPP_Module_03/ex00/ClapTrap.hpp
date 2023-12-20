c2ebb3c03ddf89f295f30ba927e9053941c29bfa
Houscéin Ab <tarzan@killyourself.local>
2023-12-20 08:24:50 +0100

    CPP______

--- CPP_Module_03/ex00/ClapTrap.hpp
+++ CPP_Module_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 00:27:55 by elakhfif          #+#    #+#             */
-/*   Updated: 2023/12/09 00:32:57 by elakhfif         ###   ########.fr       */
+/*   Updated: 2023/12/20 08:11:02 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,18 @@
 
 class ClapTrap
 {
-    protected:
+    private:
 	std::string _name;
-	int _hit_points;
-	int _max_hit_points;
-	int _energy_points;
-	int _max_energy_points;
-	int _level;
-	int _attack_damage;
+	unsigned int _hit_points;
+	unsigned int _energy_points;
+	unsigned int _attack_damage;
     public:
 	ClapTrap();
 	ClapTrap(std::string name);
 	ClapTrap(ClapTrap const &f);
 	ClapTrap &operator=(ClapTrap const &f);
 	~ClapTrap();
-	void attack(std::string const & target);
+	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
 };
