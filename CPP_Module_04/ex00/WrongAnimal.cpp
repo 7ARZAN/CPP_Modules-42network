4a28790d4514cc26ee29133576e25c87061b5a09
elhousseine lakhfif <elakhfif@c2r8p1.1337.ma>
2023-12-27 10:37:22 +0100

    CPP_____

---
+++ CPP_Module_04/ex00/WrongAnimal.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elakhfif <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 10:23:39 by elakhfif          #+#    #+#             */
+/*   Updated: 2023/12/27 10:36:56 by elakhfif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(): _type("WrongAnimal"){
+	std::cout << "WrongAnimal Constructor called" << std::endl;
+}
--- CPP_Module_04/ex00/main.cpp
+++ CPP_Module_04/ex00/main.cpp
@@ -6,18 +6,23 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 08:12:11 by elakhfif          #+#    #+#             */
-/*   Updated: 2023/12/22 08:12:45 by elakhfif         ###   ########.fr       */
+/*   Updated: 2023/12/27 10:34:25 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
 
 int main(){
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	const WrongAnimal* wrong = new WrongCat();
+	std::cout << wrong->getType() << " " << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
+	wrong->makeSound();
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
