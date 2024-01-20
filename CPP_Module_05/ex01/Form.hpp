1230f7407753a39648140d20484d2ceb9863ddb1
elhousseine lakhfif <elakhfif@c2r7p4.1337.ma>
2024-01-20 17:06:53 +0100

    Form header done.

--- CPP_Module_05/ex01/Form.hpp
+++ CPP_Module_05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 09:09:53 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/01/17 09:10:41 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/01/20 17:06:22 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,38 @@
 # include "Bureaucrat.hpp"
 
 class	Bureaucrat;
+class	Form{
+	private:
+		std::string const	_name;
+		bool				_signed;
+		int const			_grade_to_sign;
+		int const			_grade_to_execute;
+	public:
+		Form();
+		Form(std::string const, int const, int const);
+		Form(Form const &);
+		Form	&operator=(Form const &);
+		~Form();
 
+		std::string const	getName() const;
+		bool				getSigned() const;
+		int					getGradeToSign() const;
+		int					getGradeToExecute() const;
+		void				beSigned(Bureaucrat const &);
+		class	GradeTooHighException: public std::exception{
+			public:
+				virtual const char* what() const throw();
+		};
+		class	GradeTooLowException: public std::exception{
+			public:
+				virtual const char* what() const throw();
+		};
+		class	FormNotSignedException: public std::exception{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream	&operator<<(std::ostream &, Form const &);
 
 #endif
