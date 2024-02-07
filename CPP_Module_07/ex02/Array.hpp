3c0930560c11f9c9273a8da3ce80f032de8627b3
elhousseine lakhfif <elakhfif@c1r8p6.1337.ma>
2024-02-07 06:19:05 +0100

    ____CPP

--- CPP_Module_07/ex02/Array.hpp
+++ CPP_Module_07/ex02/Array.hpp
@@ -1,40 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 02:24:14 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/02/07 01:09:13 by tarzan           ###   ########.fr       */
+/*   Updated: 2024/02/07 05:41:21 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-# define ITER_HPP
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
 #include <iostream>
 
 template	<typename T>
 
-void	iter(T *array, int size, void (*f)(T const&)){
-	for (int i = 0; i < size; i++)
-		f(array[i]);
-}
-
-template	<typename T>
-
-void	add(T const &x){
-	if (x + 1)
-		std::cout << "The result of adding 1 to " << x << " is: " << x + 1 << std::endl;
-	else
-		std::cout << "Error: Can't add 1 to this type" << std::endl;
-}
-
-template	<typename T>
-
-void	print(T const &x){
-	std::cout << x << std::endl;
-}
+class Array{
+	private:
+		T		*arr;
+		unsigned int		n;
+	public:
+		Array();
+		Array(unsigned int n){
+			this->n = n;
+			this->arr = new T[n];
+		};
+		Array(const Array &src);
+		~Array();
+		Array	&operator=(const Array &src);
+		T		&operator[](unsigned int i);
+		unsigned int	size() const;
+		class OutOfLimits : public std::exception
+		{
+			public:
+				virtual const char	*what() const throw();
+		};
+};
 
 #endif
