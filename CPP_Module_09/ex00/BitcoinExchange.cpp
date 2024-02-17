9266fe6b5262a6797690efaa6eb6b574b7ecf14f
Houscéin Ab <tarzan@killyourself.local>
2024-02-17 17:27:42 +0100

    CPP________

--- CPP_Module_09/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:57:38 by tarzan            #+#    #+#             */
-/*   Updated: 2024/02/16 15:24:38 by tarzan           ###   ########.fr       */
+/*   Updated: 2024/02/17 17:27:39 by tarzan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,75 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <fstream>
+
+bool	check_file(std::string file){
+	std::ifstream f(file);
+
+	if (f.good()){
+		f.close();
+		return (true);
+	}
+	return (false);
+}
 
 bool	check_date(std::string date){
-	uint32_t year, month, day;
-	std::string delimiter = "-";
-	size_t pos = 0;
-	std::string token;
-	int i = 0;
+	std::string year = date.substr(0, 4);
+	std::string month = date.substr(5, 2);
+	std::string day = date.substr(8, 2);
+	std::string leap_year = "28";
 
-	while ((pos = date.find(delimiter)) != std::string::npos) {
-		token = date.substr(0, pos);
-		if (i == 0)
-			year = std::stoul(token); //stoul for unsigned long
-		else if (i == 1)
-			month = std::stoul(token); //stoul for unsigned long
-		date.erase(0, pos + delimiter.length());
-		i++;
+	for (int i = 0; i < 4; i++){
+		if (year[i] < '0' || year[i] > '9')
+			return (false);
 	}
-	day = std::stoul(date); //stoul for unsigned long
-	while (date.find(delimiter) != std::string::npos)
-		return (false);
-	if (year < 2010 || year > 2021)
-		return (false);
-	if (month < 1 || month > 12)
-		return (false);
-	if (day < 1 || day > 31)
+	for (int i = 0; i < 2; i++){
+		if (month[i] < '0' || month[i] > '9')
+			return (false);
+	}
+	for (int i = 0; i < 2; i++){
+		if (day[i] < '0' || day[i] > '9')
+			return (false);
+	}
+	if (month == "02"){
+		if ((std::atoi(year.c_str()) % 4 == 0 && std::atoi(year.c_str()) % 100 != 0) || std::atoi(year.c_str()) % 400 == 0)
+			leap_year = "29";
+		else
+			leap_year = "28";
+		if (day > leap_year)
+			return (false);
+	}
+	else if (month == "01" || month == "03" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12"){
+		if (day > "31")
+			return (false);
+	}
+	else if (month == "04" || month == "06" || month == "09" || month == "11"){
+		if (day > "30")
+			return (false);
+	}
+	else
 		return (false);
 	return (true);
 }
 
 int	main(int argc, char **argv){
 	if (argc != 2){
-		std::cerr << "Usage: ./BitcoinExchange <date>" << std::endl;
+		std::cerr << "Usage: ./BitcoinExchange <file>" << std::endl;
 		return (1);
 	}
-	if (!check_date(argv[1])){
+	if (!check_file(argv[1])){
+		std::cerr << "Invalid file" << std::endl;
+		return (1);
+	}
+	std::string date;
+	std::cout << "Enter the date (YYYY-MM-DD): ";
+	std::cin >> date;
+	if (!check_date(date)){
 		std::cerr << "Invalid date" << std::endl;
 		return (1);
 	}
+	// BitcoinExchange exchange(argv[1]);
+	// exchange.get_data(date);
+	// exchange.display();
 	return (0);
 }
