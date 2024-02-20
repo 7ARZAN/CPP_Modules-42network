0207b5d281919f55e5751c5da7b5de7bc8473818
elhousseine lakhfif <elakhfif@c2r9p1.1337.ma>
2024-02-20 20:38:44 +0100

    CPP____

--- CPP_Module_09/ex00/main.cpp
+++ CPP_Module_09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 15:20:43 by tarzan            #+#    #+#             */
-/*   Updated: 2024/02/19 03:38:18 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/02/20 20:38:09 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 #include <fstream>
 
 int	main(int ac, char **av){
-	if (ac != 3)
-	{
-		std::cerr << "Usage: " << av[0] << " <bitcoin_file>" << " <how_much_bitcoin>" << std::endl;
+	if (ac != 3){
+		std::cerr << "Usage: " << av[0] << " <b1tc01n_file>" << " <how_much_b1tc01n>" << std::endl;
 		return (1);
 	}
 	std::ifstream file(av[1]);
 	if (!file.is_open()){
-		std::cerr << "Error: file not found" << std::endl;
+		std::cerr << "ERROR: file not found" << std::endl;
 		return (1);
 	}
-	// call file parser here and pass the file to it "FileParser"
-	// and then pass the parsed data to the BitcoinExchange class "BitcoinExchange"
-	// and then call the method "getBestPrice" to get the best price "if the date exists if not get the closest date"
-	// and then print the result
+	//			TODO:
+	//check if " av[2] " is positive number between [0 - 1000].
+	//check the file given if he has the correct infos.
+	//check if data file is present in your directory.
+	//search for the date given and return it.
+	//error handling [input errors && data file errors].
+	//create function printData() to print the data results.
 	return (0);
 }
