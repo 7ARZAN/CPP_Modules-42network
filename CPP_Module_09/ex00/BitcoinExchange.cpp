cf76cc6db26cd4436586bbc1c42dd09d5591a43e
Houscéin Ab <tarzan@killyourself.local>
2024-02-23 05:04:52 +0100

    CPP________

--- CPP_Module_09/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 13:57:38 by tarzan            #+#    #+#             */
-/*   Updated: 2024/02/22 20:16:33 by tarzan           ###   ########.fr       */
+/*   Updated: 2024/02/22 21:39:28 by tarzan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 #include <cstdint>
 #include <fstream>
 
-// bool	third_argument(std::string arg){
-// 	if (arg.length() > 4)
-// 		return (std::cout << "ERROR: The third argument must be a number between 0 and 1000" << std::endl, false);
-// 	for (int i = 0; i < arg.length(); i++)
-// 		if (arg[i] < '0' || arg[i] > '9' || arg > "1000")
-// 			return (std::cout << "ERROR: The third argument must be a number between 0 and 1000" << std::endl, false);
-// 	return (true);
-// }
+bool	third_argument(std::string arg){
+	if (arg.length() > 4)
+		return (std::cout << "ERROR: The third argument must be a number between 0 and 1000" << std::endl, false);
+	for (int i = 0; i < arg.length(); i++)
+		if (arg[i] < '0' || arg[i] > '9' || (arg.length() > 3 && arg > "1000"))
+			return (std::cout << "ERROR: The third argument must be a number between 0 and 1000" << std::endl, false);
+	return (true);
+}
 //
 // bool	check_date(std::string date);
 // void	check_input_data(std::string data){
@@ -55,45 +55,7 @@
 // 		}
 // 	}
 // }
-//
-// bool	check_date(std::string date){
-// 	std::string year = date.substr(0, 4);
-// 	std::string month = date.substr(5, 2);
-// 	std::string day = date.substr(8, 2);
-// 	std::string leap_year = "28";
-//
-// 	for (int i = 0; i < 4; i++){
-// 		if (year[i] < '0' || year[i] > '9')
-// 			return (false);
-// 	}
-// 	for (int i = 0; i < 2; i++){
-// 		if (month[i] < '0' || month[i] > '9')
-// 			return (false);
-// 	}
-// 	for (int i = 0; i < 2; i++){
-// 		if (day[i] < '0' || day[i] > '9')
-// 			return (false);
-// 	}
-// 	if (month == "02"){
-// 		if ((std::atoi(year.c_str()) % 4 == 0 && std::atoi(year.c_str()) % 100 != 0) || std::atoi(year.c_str()) % 400 == 0)
-// 			leap_year = "29";
-// 		else
-// 			leap_year = "28";
-// 		if (day > leap_year)
-// 			return (false);
-// 	}
-// 	else if (month == "01" || month == "03" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12"){
-// 		if (day > "31")
-// 			return (false);
-// 	}
-// 	else if (month == "04" || month == "06" || month == "09" || month == "11"){
-// 		if (day > "30")
-// 			return (false);
-// 	}
-// 	else
-// 		return (false);
-// 	return (true);
-// }
+
 //
 // void	printData(std::string date, std::string price){
 // 	std::cout << "Date: " << date << " Price: " << price << std::endl;
@@ -178,7 +140,7 @@ bool	BitcoinExchange::ValidateDate(const std::string &date){
 
 bool	BitcoinExchange::ValidateValue(double value){
 	if (value < 0.0 || value > 1000.0) {
-		std::cerr << "Error: Value must be between 0 and 1000 => "
+		std::cerr << "ERROR: Value must be between 0 and 1000 => "
 			  << value << '\n';
 		return false;
 	}
@@ -205,19 +167,19 @@ void	BitcoinExchange::Process_input(const std::string &filename){
 		char delimiter;
 
 		if (!(ss >> date >> delimiter >> value)) {
-			std::cerr << "Error: Unable to parse line => "
+			std::cerr << "ERROR: Unable to parse line => "
 				  << line << '\n';
 			continue;
 		}
 
 		if (delimiter != '|') {
-			std::cerr << "Error: Expected '|' delimiter in line => "
+			std::cerr << "ERROR: Expected '|' delimiter in line => "
 				  << line << '\n';
 			continue;
 		}
 
 		if (!ValidateDate(date)) {
-			std::cerr << "Error: Invalid date format in line => "
+			std::cerr << "ERROR: Invalid date format in line => "
 				  << line << '\n';
 			continue;
 		}
@@ -228,7 +190,7 @@ void	BitcoinExchange::Process_input(const std::string &filename){
 
 		double exchangeRate = GetExchangeRate(date);
 		if (exchangeRate == -1.0) {
-			std::cerr << "Error: No exchange rate available for date => "
+			std::cerr << "ERROR: No exchange rate available for date => "
 				  << date << '\n';
 			continue;
 		}
@@ -240,10 +202,11 @@ void	BitcoinExchange::Process_input(const std::string &filename){
 }
 
 int	main(int ac, char **av){
-	if (ac != 3){
-		std::cerr << "Usage: " << av[0] << " <input_file>" << " <how_much_b1tc01n>" << std::endl;
+
+	if (ac != 3)
+		return (std::cerr << "Usage: " << av[0] << " <input_file>" << " <how_much_b1tc01n>" << std::endl, 1);
+	if (third_argument(av[2]) == false)
 		return (1);
-	}
 	try{
 		BitcoinExchange exchange;
 		exchange.Process_input(av[1]);
