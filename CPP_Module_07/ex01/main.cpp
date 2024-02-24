b65c21f1562c2f56ad876af62848ecc5abe0c9a1
elhousseine lakhfif <elakhfif@c1r3p2.1337.ma>
2024-02-24 08:25:55 +0100

    CPP_____

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,15 +6,47 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 02:24:05 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/02/21 23:30:36 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/02/24 08:24:36 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "iter.hpp"
 
-int	main(){
-	char		tab[] = {'a', 'b', 'c', 'd', 'e'};
-	iter(tab, 5, print);
-	iter(tab, 5, add);
-	return 0;
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
 }
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
+}
+
+// int	main(){
+// 	char		tab[] = {'a', 'b', 'c', 'd', 'e'};
+// 	iter(tab, 5, print);
+// 	iter(tab, 5, add);
+// 	return 0;
+// }
--- CPP_Module_07/ex02/main.cpp
+++ CPP_Module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 05:35:03 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/02/21 23:34:16 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/02/24 08:25:23 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,31 +61,82 @@
 //     return 0;
 // }
 
-void execute_array()
+// void execute_array()
+// {
+//     try
+//     {
+//         Array<std::string> myArray(10);
+//         for (unsigned int i = 0; i < myArray.size(); i++)
+//         {
+//             std::string newStr;
+//             newStr = "T[] = ";
+//             newStr += std::to_string(i + 1);
+//             myArray[i] = newStr;
+//         }
+//         for (unsigned int i = 0; i < myArray.size(); i++)
+//             std::cout << myArray[i] << std::endl;
+//         std::cout << myArray[22] << std::endl;
+//     }
+//     catch (const std::exception &e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+// }
+//
+// int main(void)
+// {
+//     execute_array();
+//     system("leaks Array");
+//     return 0;
+// }
+
+#define MAX_VAL 750
+int main(int, char**)
 {
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
     try
     {
-        Array<std::string> myArray(10);
-        for (unsigned int i = 0; i < myArray.size(); i++)
-        {
-            std::string newStr;
-            newStr = "T[] = ";
-            newStr += std::to_string(i + 1);
-            myArray[i] = newStr;
-        }
-        for (unsigned int i = 0; i < myArray.size(); i++)
-            std::cout << myArray[i] << std::endl;
-        std::cout << myArray[22] << std::endl;
+        numbers[-2] = 0;
     }
-    catch (const std::exception &e)
+    catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
-}
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-int main(void)
-{
-    execute_array();
-    system("leaks Array");
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
