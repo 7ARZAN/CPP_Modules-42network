10f0608eefd1d9f7fd5f0bf493dfd2a1cf394cdc
Houscéin Ab <tarzan@killyourself.local>
2024-06-18 18:25:46 +0100

    CPP______

--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 12:36:57 by tarzan            #+#    #+#             */
-/*   Updated: 2024/02/10 12:41:07 by tarzan           ###   ########.fr       */
+/*   Updated: 2024/06/18 18:20:14 by tarzan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 #include <vector>
 
 int	main(){
-	std::vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
+	std::vector<int>	vector;
+	vector.push_back(1);
+	vector.push_back(2);
+	vector.push_back(3);
+	vector.push_back(4);
+	vector.push_back(5);
 	try{
-		easyfind(v, 4);
-		std::cout << "value is: " << easyfind(v, 4) << std::endl;
+		easyfind(vector, 4);
+		std::cout << "value is: " << easyfind(vector, 4) << std::endl;
 	}
 	catch(const std::exception& e){
 		std::cerr << e.what() << '\n';
 	}
 	try{
-		easyfind(v, 1);
-		std::cout << "value is: " << easyfind(v, 1) << std::endl;
+		easyfind(vector, 1);
+		std::cout << "value is: " << easyfind(vector, 1) << std::endl;
 	}
 	catch(const std::exception& e){
 		std::cerr << e.what() << '\n';
