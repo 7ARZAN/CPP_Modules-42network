d97b8efa91f589c71f136d9adb3fcb1bb13c709b
elhousseine lakhfif <elakhfif@c1r6p1.1337.ma>
2024-07-28 06:04:25 +0100

    CPP________

--- CPP_Module_09/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/BitcoinExchange.cpp
@@ -6,158 +6,126 @@
 /*   By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 01:48:16 by tarzan            #+#    #+#             */
-/*   Updated: 2024/07/28 01:15:34 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/07/28 06:01:49 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 #include <sstream>
 #include <fstream>
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <map>
 
+float	BitcoinExchange::stringToFloat(const std::string &value){
+	float	result;
 
-static float stringToFloat(const std::string &value)
-{
-	float			result;
-	std::stringstream	ss(value);
-
+	std::stringstream ss(value);
 	ss >> result;
-	return (result);	
+	return result;
 }
 
-static bool	isAllDigits(const std::string &value)
-{
-	size_t	dotCount = 0;
-	size_t	digitCount = 0;
-	size_t	i = 0;
+bool	BitcoinExchange::isAllDigits(const std::string &value){
+    bool	dotCounter;
+    size_t	i;
 
-	while (i < value.length()){
-		if (value[i] == '.')
-			dotCount++;
-		if (isdigit(value[i]))
-			digitCount++;
-		i++;
-	}
-	if (dotCount > 1 || digitCount != (value.length() - dotCount))
-		return (false);
-	return (true);
+    i = 0;
+    dotCounter = false;
+    while (value[i]){
+	if (value[i] == '.' && value[i + 1] && !dotCounter)
+	    dotCounter = true;
+	else if (!isdigit(value[i]))
+	    return false;
+	i++;
+    }
+    return true;
 }
 
-static bool	isValidDate(const std::string &date){
+bool	BitcoinExchange::isValidDate(const std::string &date){
 	std::string	year;
 	std::string	month;
 	std::string	day;
+	size_t		FirstHyphen;
+	size_t		SecondHyphen;
 
-	if (date.length() != 10)
-	{
-		std::cerr << "ERROR: invalid date format.\n";
-		return (false);
-	}
-	year = date.substr(0, 4);
-	month = date.substr(5, 2);
-	day = date.substr(8, 2);
+	FirstHyphen = date.find("-");
+	SecondHyphen = date.find("-", FirstHyphen + 1);
+	year = date.substr(0, FirstHyphen);
+	month = date.substr(FirstHyphen + 1, SecondHyphen - FirstHyphen - 1);
+	day = date.substr(SecondHyphen + 1);
 	if (!isAllDigits(year) || !isAllDigits(month) || !isAllDigits(day))
-	{
-		std::cerr << "ERROR: invalid date format.\n";
 		return (false);
-	}
-	if (stoi(year) < 2000 || stoi(year) > 2021)
-	{
-		std::cerr << "ERROR: invalid year.\n";
+	if (year.length() != 4)
 		return (false);
-	}
-	if (stoi(month) < 1 || stoi(month) > 12)
-	{
-		std::cerr << "ERROR: invalid month.\n";
+	if (month.length() != 2 || (int)stringToFloat(month) < 1 || (int)stringToFloat(month) > 12)
 		return (false);
-	}
-	if (stoi(day) < 1 || stoi(day) > 31)
-	{
-		std::cerr << "ERROR: invalid day.\n";
+	if (day.length() != 2 || (int)stringToFloat(day) < 1 || (int)stringToFloat(day) > 31)
 		return (false);
-	}
 	return (true);
 }
 
-static bool	isValidValue(const std::string &value)
-{
-	float	realValue;
-	
-	if (!isAllDigits(value))
-	{
-		std::cerr << "ERROR: invalid value format.\n";
+bool	BitcoinExchange::isValidValue(const std::string &value){
+	float	temp;
+
+	temp = stringToFloat(value);
+	if (temp < 0){
+		std::cerr << "ERROR: not a positive number.\n";
 		return (false);
 	}
-	realValue = stringToFloat(value);
-	if (realValue < 0)
-	{
-		std::cerr << "ERROR: invalid value.\n";
+	if (temp > 1000){
+		std::cerr << "ERROR: too large of a number.\n";
 		return (false);
 	}
 	return (true);
+
 }
 
-static std::string trim(const std::string &str, const std::string &charList)
-{
-	size_t head = str.find_first_not_of(charList);
-	size_t tail = str.find_last_not_of(charList);
+std::string	BitcoinExchange::trim(const std::string &str, const std::string &charList){
+	size_t	Head;
+	size_t	Tail;
 
-	if (head != std::string::npos)
-		return (str.substr(head, ++tail - head));
+	Head = str.find_first_not_of(charList);
+	Tail = str.find_last_not_of(charList);
+	if (Head != std::string::npos)
+		return (str.substr(Head, ++Tail - Head));
 	return ("");
 }
 
-BitcoinExchange::BitcoinExchange(void)
-{
-	std::string		key;
-	std::string		value;
-	std::string		line;
+BitcoinExchange::BitcoinExchange(){
 	std::ifstream	data("./data.csv");
-
-	if (!data.is_open())
-	{
+	std::string	row;
+	std::string	date;
+	
+	if (!data.is_open()){
 		std::cerr << "ERROR: unable to access Database.\n";
 		exit(1);
 	}
-	getline(data, line);
-	while (getline(data, line))
-	{
-		key = line.substr(0, line.find(","));
-		value = line.substr(line.find(",") + 1);
-		this->data[key] = stringToFloat(value);
+	getline(data, row);
+	while (getline(data, row)){
+		date = row.substr(0, row.find(","));
+		this->data[date] = stringToFloat(row.substr(row.find(",") + 1));
 	}
 	data.close();
 }
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &origin)
-{
-	*this = origin;
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &origin) : data(origin.data){
 }
 
-BitcoinExchange::~BitcoinExchange(void)
-{
-	
+BitcoinExchange::~BitcoinExchange(){
+
 }
 
-BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &origin)
-{
-	if (this == &origin)
-		this->data = origin.data;
-	return (*this);
-}
+double BitcoinExchange::getBitcoinPrice(const std::string &date){
+	float	result;
+	std::map<std::string, float>::const_iterator it;
 
-double	BitcoinExchange::getBitcoinPrice(const std::string &date)
-{
-	float							result = 0;
-	std::map<std::string, double>::iterator it;
-	
-	if (!isValidDate(date))
-	{
+	result = 0;
+	if (!isValidDate(date)){
 		std::cerr << "ERROR: Bad input => " << date << "\n";
-		return (-1); 
+		return (-1);
 	}
-	
-	for (it = this->data.begin(); it != data.end(); it++)
-	{
+	for (it = this->data.begin(); it != data.end(); it++){
 		if (it->first > date)
 			break;
 		if (it->first <= date)
@@ -166,36 +134,31 @@ double	BitcoinExchange::getBitcoinPrice(const std::string &date)
 	return (result);
 }
 
-void BitcoinExchange::handleTables(const std::string &path)
-{
+void BitcoinExchange::handleTables(const std::string &path) {
 	float			exchangerate;
-	std::string		key;
+	std::string		date;
 	std::string		value;
-	std::string		line;
+	std::string		row;
 	std::ifstream	data(path.c_str());
 
-	if (!data.is_open())
-	{
+	if (!data.is_open()){
 		std::cerr << "ERROR: unable to access file.\n";
 		return ;
 	}
-	getline(data, line);
-	if (line != "date | value")
-	{
+	getline(data, row);
+	if (row != "date | value"){
 		std::cerr << "ERROR: invalid file format.\n";
 		data.close();
 		return ;
 	}
-	while (getline(data, line))
-	{
-		key = trim(line.substr(0, line.find("|")), " ");
-		value = trim(line.substr(line.find("|") + 1), " ");
-		exchangerate = getBitcoinPrice(key);
-
+	while (getline(data, row)){
+		date = trim(row.substr(0, row.find("|")), " ");
+		value = trim(row.substr(row.find("|") + 1), " ");
+		exchangerate = getBitcoinPrice(date);
 		if (exchangerate < 0)
 			continue;
 		if (isValidValue(value))
-			std::cout << key << " => " << value << " = " << exchangerate * stringToFloat(value) << "\n";
+			std::cout << date << " => " << value << " = " << exchangerate * stringToFloat(value) << "\n";
 	}
 	data.close();
 }
