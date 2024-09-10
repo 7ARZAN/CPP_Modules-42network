2aebce146ae00906403df1342007b26035e63739
Houscéin Ab <tarzan@killyourself.local>
2024-09-10 05:44:55 +0100

    CPP________

--- CPP_Module_09/ex02/PmergeMe.cpp
+++ CPP_Module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 04:38:50 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/08/07 05:41:54 by tarzan           ###   ########.fr       */
+/*   Updated: 2024/09/10 05:15:42 by tarzan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,16 @@ std::vector<int>	PmergeMe::binaryInsertion(int number, std::vector<int> &list){
 
 PmergeMe::pair_vector	PmergeMe::createPairs(const std::vector<int> &list){
 	pair_vector	pairs;
+	size_t		index;
 
-	for (size_t i = 0; i < list.size(); i += 2){
-		if (i + 1 < list.size())
-			pairs.push_back(std::make_pair(list[i], list[i + 1]));
+
+	index = 0;
+	while (index < list.size()){
+		if (index + 1 < list.size())
+			pairs.push_back(std::make_pair(list.at(index), list.at(index + 1)));
 		else
-			pairs.push_back(std::make_pair(list[i], -1));
+			pairs.push_back(std::make_pair(list.at(index), -1));
+		index += 2;
 	}
 	// for (size_t i = 0; i < pairs.size(); i++){
 	// 	std::cout << "[ " << pairs[i].first << " " << pairs[i].second << " ]" << std::endl;
@@ -61,7 +65,7 @@ void	PmergeMe::sortPairs(pair_vector &pairs, std::vector<int> &list){
 
 	i = 0;
 	while (i < pairs.size()){
-		pair = pairs[i];
+		pair = pairs.at(i);
 		if (pair.second == -1){
 			mainChain.push_back(pair.first);
 			i++;
@@ -88,29 +92,12 @@ void	PmergeMe::sortPairs(pair_vector &pairs, std::vector<int> &list){
 
 std::vector<int>	PmergeMe::FordJohnsonAlgorithm(std::vector<int> &list2Sort){
 	std::vector<int>	mainChain;
-	std::vector<int>	pending;
 	pair_vector		pairs;
-	
+
 	pairs = createPairs(list2Sort);
 	for (size_t i = 0; i < pairs.size(); i++){
-		if (pairs[i].second == -1)
-			pending.push_back(pairs[i].first);
+		std::cout << "[ " << pairs[i].first << " " << pairs[i].second << " ]" << std::endl;
 	}
 	sortPairs(pairs, mainChain);
-	while (pending.size() > 0){
-		mainChain = binaryInsertion(pending[0], mainChain);
-		pending.erase(pending.begin());
-	}
-	for (size_t i = 0; i < mainChain.size(); i++){
-		std::cout << mainChain[i] << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "----------------------" << std::endl;
-
-	for (size_t i = 0; i < list2Sort.size(); i++){
-		std::cout << list2Sort[i] << " ";
-	}
-	std::cout << std::endl;
 	return (mainChain);
 }
