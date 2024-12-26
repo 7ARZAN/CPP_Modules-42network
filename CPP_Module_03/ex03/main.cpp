e56c8a0c56910ce9da58abc26ebaf5b97766fcb3
elhousseine lakhfif <akaouam1@gmail.com>
2024-12-26 16:22:26 +0100

    CPP______

--- CPP_Module_03/ex03/main.cpp
+++ CPP_Module_03/ex03/main.cpp
@@ -6,17 +6,18 @@
 /*   By: elakhfif <elakhfif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 02:44:44 by elakhfif          #+#    #+#             */
-/*   Updated: 2024/12/22 15:33:46 by elakhfif         ###   ########.fr       */
+/*   Updated: 2024/12/26 16:21:45 by elakhfif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 int main(){
-    DiamondTrap tarzan("tarzan");
+    DiamondTrap	tarzan("tarzan");
+
     tarzan.attack("jane");
-    tarzan.takeDamage(10);
-    tarzan.beRepaired(10);
+    tarzan.takeDamage(1000);
+    tarzan.beRepaired(1000);
     tarzan.guardGate();
     tarzan.highFivesGuys();
     tarzan.whoAmI();
